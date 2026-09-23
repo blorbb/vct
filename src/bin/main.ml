196196cb4bd02bc15df669f3f478bc2546d03b7d
blorbb <88137137+blorbb@users.noreply.github.com>
2026-09-23 12:45:22 +1000

    use spec solver

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -5,8 +5,8 @@ let parse_file path =
 ;;
 
 let check fml =
-  let result = Vct.Solver.NoModel.solve_fml fml in
-  match Vct.Solver.NoModel.Solution.is_sat result with
+  let result = Vct.Solver.Spec.solve_fml fml in
+  match Vct.Solver.Spec.Solution.is_sat result with
   | true -> print_endline "SAT"
   | false -> print_endline "UNSAT"
 ;;
